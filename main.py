76dbad2e45d019797e39f40b7407f80f769707a7
Rawinan <rawinan888@gmail.com>
2025-03-04 23:34:08 +0700

    clear data

--- main.py
+++ main.py
@@ -10,30 +10,35 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import nltk
 
+nltk.download('stopwords')
+# print(stopwords.words('english'))
+
 # Load dataset
 data = pd.read_csv('train.csv')  # Dataset ของคุณ
 data = data.dropna()  # ลบข้อมูลที่เป็นค่าว่าง
+print(data.shape)
+print(data.head)
 
-# Split data into input (X) and output (y)
-X = data['text']
-y = data['label']  # 1 = Fake, 0 = Real
+# # Split data into input (X) and output (y)
+# X = data['text']
+# y = data['label']  # 1 = Fake, 0 = Real
 
-# Text preprocessing with TF-IDF
-vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
-X = vectorizer.fit_transform(X)
+# # Text preprocessing with TF-IDF
+# vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
+# X = vectorizer.fit_transform(X)
 
-# Split data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# # Split data into train and test sets
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Train a logistic regression model
-model = LogisticRegression()
-model.fit(X_train, y_train)
+# # Train a logistic regression model
+# model = LogisticRegression()
+# model.fit(X_train, y_train)
 
-# Predict and evaluate
-y_pred = model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy:.2f}")
+# # Predict and evaluate
+# y_pred = model.predict(X_test)
+# accuracy = accuracy_score(y_test, y_pred)
+# print(f"Accuracy: {accuracy:.2f}")
 
-joblib.dump(model, 'model.pkl')  # บันทึกโมเดลลงไฟล์
-joblib.dump(vectorizer, 'vectorizer.pkl')  # บันทึก TF-IDF Vectorizer
-print("Model saved successfully!")
+# joblib.dump(model, 'model.pkl')  # บันทึกโมเดลลงไฟล์
+# joblib.dump(vectorizer, 'vectorizer.pkl')  # บันทึก TF-IDF Vectorizer
+# print("Model saved successfully!")
