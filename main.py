5b4e52725778e0c939aec1a72f835e84a0fedcbf
Rawinan <rawinan888@gmail.com>
2025-03-04 22:28:05 +0700

    import

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import joblib
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import nltk
 
 # Load dataset
 data = pd.read_csv('train.csv')  # Dataset ของคุณ
